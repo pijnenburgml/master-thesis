c2f69463b76ed35b850b96b86c4362d4bc3e935d
pijnenburgml <marielou.pijnenburg2000@gmail.com>
2023-08-15 11:07:07 +0200

    Modified script by Marie-Lou First code tryout for parallel processing Different target areas

--- script/tile_and_parallel_processing_example_JJA.r
+++ script/tile_and_parallel_processing_example_JJA.r
@@ -8,6 +8,9 @@ library(ggplot2)
 library(cowplot)
 library(terra)
 library(tidyterra)
+library(devtools)
+library(gdalUtils)
+
 setwd("~/data")
 area_interest <- st_read("areas_of_interest.gpkg")
 area_interest_proj <- st_transform(area_interest,32613)
@@ -15,9 +18,24 @@ sent_aoi_stack_crop <- rast("output/sent_crop_view.tif")
 ext_aoi <- ext(area_interest_proj)
 ext_sent <- ext(sent_aoi_stack_crop)
 
-target_area <- data.frame(x = c(476480, 517470, 517470, 476480),
-                          y = c(7657730, 7657730,  7687110, 7687110)) %>%
+
+#########
+# Target area covering the whole Aviris fligth stripe 
+#########
+target_area <- data.frame(x = c(473557.1, 569724.9, 569724.9, 473557.1), #this is the Sentinel-2 outline.
+                          y = c(7639904, 7639904, 7700841 ,7700841)) %>%
   st_as_sf(
+    coords = c("x", "y"),
+    crs = 32613 # UTM Zone 12 -> so coordiantes are in metres
+  ) %>% summarize() %>%
+  st_convex_hull()
+
+#########
+# Target area covering the aoi --> Sentinel tile
+#########
+target_area <- data.frame(x = c(476480, 517470, 517470, 476480), #this is the Sentinel-2 outline.
+                          y = c(7657730, 7657730,  7687110, 7687110)) %>%
+    st_as_sf(
     coords = c("x", "y"),
     crs = 32613 # UTM Zone 12 -> so coordiantes are in metres
   ) %>% summarize() %>%
@@ -106,13 +124,23 @@ ggplot() +
   theme(legend.position = "none")
   # +geom_spatraster(data=mask, alpha=0.3)
 
+#Save the tile boundaries as shapefile 
+# st_write(grid_polygons, "~/data/output/tilling/grid_polygons_aoi.shp")
+# st_write(grid_polygons, "~/data/output/tilling/grid_polygons_aviris.shp")
+
 ##############################################################
+
+###################
+# Get the fligth stripe boundaries
+###################
+
 boundary_dir <- "~/data/Aviris_data/boundaries"
 boundaries <- list.files(path=boundary_dir, pattern="KML", full.names = T)
 try <- st_read("~/data/Aviris_data/boundaries/ang20190801t143347_outline_KML.kml")
 try_proj <- st_transform(try,32613)
 st_intersects(grid_polygons[1,], try_proj$geometry)
 
+#plot enablling to tell which tile cell overlap with a flight stip (here 3347 --> try_proj)
 ggplot(data=grid_polygons) +
   geom_sf(
     # aes(fill = cell_id, alpha=0.1)
@@ -121,8 +149,30 @@ ggplot(data=grid_polygons) +
   geom_sf(data=try_proj, aes(fill="red"))
 st_intersects(grid_polygons[grep(pattern="x_1_y_17", grid_polygons$cell_id),], try_proj)
 
+########
+# Try to apply gdal warp to get a piece of fligth strip rectified I could run the random forest on
+########
+
+# try <- st_read("~/data/Aviris_data/boundaries/ang20190801t160747_outline_KML.kml")
+# try_proj <- st_transform(try,32613)
+# ggplot(data=grid_polygons) +
+#   geom_sf(
+#     # aes(fill = cell_id, alpha=0.1)
+#   ) +
+#   geom_sf_label(aes(label=cell_id), cex=0.8)+
+#   geom_sf(data=try_proj, aes(fill="red"))
+# 
+# # x_35_y_17
+# source_path <-"~/scratch/reflectance_data/ang20190801t160747_rfl" 
+# tile_path <- "~/data/output/tilling/grid_polygons_aoi.shp"
+# filter(grid_polygons, cell_id=="x_35_y_17") %>% st_write("~/data/output/tilling/grid_polygons_x_35_y_17.shp")
+# tile_path <- "~/data/output/tilling/grid_polygons_x_35_y_17.shp"
+# out_file <- paste0(tempfile(), ".hdr")
+# 
+# sf::gdal_utils(util="warp", source=source_path, destination=out_file, options=c("cutline", tile_path, "of", "ENVI"))
+
 ######################
-# make multipolygon from all the fligth strip boundaries
+# make multipolygon from all the flight strip boundaries
 ######################
 
 boundaries_sf <- lapply(boundaries, FUN=st_read)
@@ -148,6 +198,41 @@ boundaries_multipolygon <- c(boundaries_sf[[1]]$geometry, boundaries_sf[[2]]$geo
   boundaries_sf[[13]]$geometry,boundaries_sf[[14]]$geometry,boundaries_sf[[15]]$geometry,boundaries_sf[[16]]$geometry)%>% st_cast("MULTIPOLYGON")
 plot(boundaries_multipolygon)
 names(boundaries_multipolygon) <- boundaries_name
+
+###############
+# Using everything for parallel processing
+###############
+
+cl <- makeCluster(2)
+
+# then load any libraries you might need on the cluster
+clusterEvalQ(cl, {
+  library(sf)
+  library(dplyr)
+  library(terra)
+})
+
+# and export any variables you need
+clusterExport(cl, c("grid_polygons", "boundaries_multipolygon"))
+
+# then run things in parallel 
+pblapply(grid_polygons$cell_id,
+         function(cell) {
+           idx <- which(st_intersects(boundaries_multipolygon, filter(grid_polygons, cell_id==cell), sparse = F)==T)
+           file <- names(boundaries_multipolygon[idx])
+           if (length(file)>0) {
+             idx_file <- list.files("~/data/Aviris_data/boundaries/") %>% grep(pattern=paste(substr(file,0,18),collapse="|")) #to be change to real file folder and not boundaries
+             crop_rasters <- list()
+             for (x in idx_file) {
+                y <- rast(paste("~/data/Aviris_data/boundaries/", list.files("~/data/Aviris_data/boundaries/")[x], sep="")) # might need to be changed into an sf::gdal function 
+                crop_rasters[match(x, idx_file)]<-crop(y, ext(filter(grid_polygons, cell_id==cell)))
+              }
+             mosaic(crop_rasters) %>%  writeRaster(filename = cell)#Need to write the mosaic tiled there?
+           }
+         },
+         cl = cl # the name of your cluster
+)
+
 idx <- which(st_intersects(boundaries_multipolygon, filter(grid_polygons, cell_id=="x_40_y_30"), sparse = F)==T)
 file <- names(boundaries_multipolygon[idx])
 
@@ -158,11 +243,30 @@ plot(boundaries_multipolygon, add=T, col="grey")
 if (length(file)>0) {
   idx_file <- list.files("~/data/Aviris_data/boundaries/") %>% grep(pattern=paste(substr(file,0,18),collapse="|")) #to be change to real file folder and not boundaries
   paste("~/data/Aviris_data/boundaries/", list.files("~/data/Aviris_data/boundaries/")[idx_file], sep="")
-    for (x in 1:length(idx_file)) {
-      st_read(paste("~/data/Aviris_data/boundaries/", list.files("~/data/Aviris_data/boundaries/")[x], sep=""))
+    for (x in idx_file) {
+      y <- rast(paste("~/data/Aviris_data/boundaries/", list.files("~/data/Aviris_data/boundaries/")[x], sep=""))
+      crop(y, ext(filter(grid_polygons, cell_id==cell_id)))
   }
 }
 
+# lapply(idx_file, FUN=function(x){y <- rast(paste("~/data/Aviris_data/boundaries/", list.files("~/data/Aviris_data/boundaries/")[x], sep=""))
+#                                                   crop(y, ext(filter(grid_polygons, cell_id==cell)))})
+
+
+rectified_data <- rast("./Aviris_data/strip_3328_cropped_rect.envi")
+ggplot(data=grid_polygons) +
+  geom_sf(
+    # aes(fill = cell_id, alpha=0.1)
+  ) +
+  geom_sf_label(aes(label=cell_id), cex=0.8)+
+  geom_spatraster_contour_filled(data=rectified_data$`376.719576 Nanometers`, aes(fill="red"))
+
+crop(rectified_data, ext(filter(grid_polygons, cell_id=="x_19_y_23")))
+# working
+
+###############
+# Code from Jakob
+################
 
 # Quick example on how to iterate about tiles in parallel
 # For this I prefer using the library plapply
@@ -219,3 +323,59 @@ stopCluster(cl)
 # wrap the rast object from terra (see ?wrap)
 
 # Hope this helps!
+
+#######
+# Masking Aviris data in R
+#######
+
+# Clouds
+RF_model <- randomForest(x=training_data[,2:426], y=training_data$ID)
+pix_class <- terra::predict(object=tile, model=RF_model)
+
+
+# Shade
+strip_4159_crop <- rast("./Aviris_data/strip_4159_cropped_2_rect")
+NIR_idx <- names(strip_4159_crop) %>% grep(pattern="^8")
+NIR_average <- mean(strip_4159_crop[[NIR_idx]])
+hist(NIR_average, breaks=seq(0,0.35, by=0.01))
+# cut hard decision, at 0.1 can make sense but less as well, something like 0.05 could also be justifiable.  
+shade_mask <- ifel(NIR_average<0.05, 0, 1)
+plot(shade_mask)
+# NDWI
+green_idx <- names(strip_4159_crop) %>% grep(pattern="^5")
+green_average <- mean(strip_4159_crop[[green_idx]])
+NDWI <- (green_average-NIR_average)/(green_average+NIR_average)
+plot(NDWI)
+hist(NDWI)
+water_mask <- ifel(NDWI>0.1, 0, 1)
+plot(water_mask)
+par(mfrow=c(1,2))
+plot(shade_mask)
+plot(water_mask)
+plotRGB(strip_4159_crop, r=names(strip_4159_crop)[54], g=names(strip_4159_crop)[36], b=names(strip_4159_crop)[20], stretch="lin")
+
+
+
+############
+# Rectify/rotate data tryout
+############
+rotated_data <- rast("./Aviris_data/strip_3328_cropped_rotated_2")
+plot()
+plot(ext(rotated_data))
+
+plot(ext(sent_aoi_stack_crop))
+plot(ext(rotated_data), add=T)
+plotRGB(rotated_data, r=names(rotated_data)[54], g=names(rotated_data)[36], b=names(rotated_data)[20], stretch="lin", add=T)
+rectified_data <- rast("./Aviris_data/strip_3328_cropped_rect.envi")
+plot(ext(rectified_data), add=T)
+plotRGB(rectified_data, r=names(rectified_data)[54], g=names(rectified_data)[36], b=names(rectified_data)[20], stretch="lin", add=T)
+
+rotated_data <- rast("./Aviris_data/strip_4159_cropped_2_rotated.dat")
+rectified_data <- rast("./Aviris_data/strip_4159_cropped_2_rect")
+# plot(ext(sent_aoi_stack_crop))
+plot(ext(rotated_data), add=T)
+plot(ext(rectified_data), add=T)
+
+
+
+
