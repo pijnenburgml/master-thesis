f52d94d9e9bc3cc4dbd325ca1ed8408917daf092
pijnenburgml <marielou.pijnenburg2000@gmail.com>
2023-09-26 14:44:02 +0200

    script made to resample the spectral species map at 5m resolution

--- script/biodivmapR_aviris.R
+++ script/biodivmapR_aviris.R
@@ -13,28 +13,18 @@ library(dissUtils)
 library(biodivMapR)
 library(tools)
 library(patchwork)
-################################################
-# rectify envi file + save in the right format
-################################################
-non_rect <- rast("~/scratch/strip_4159_cropped_2")
-yes_rect <- rectify(non_rect)
-# writeRaster(yes_rect, filename = "~/data/Aviris_data/strip_4159_cropped_2_rect", filetype="ENVI", gdal=c("INTERLEAVE=BIL"), overwrite=T)
-
-non_rect <- rast("~/scratch/strip_4159_cropped_2_merged_wa_sh_mask")
-yes_rect <- rectify(non_rect)
-writeRaster(yes_rect, filename = "~/data/Aviris_data/strip_4159_cropped_2_merged_wa_sh_mask_rect", filetype="ENVI", gdal=c("INTERLEAVE=BIL"), overwrite=T, datatype="INT1U")
 
 ################################################################################
 # Variable definition
 ################################################################################
-Datadir <- "Aviris_data"
+Datadir <- "/scratch/mpijne/reflectance_data"
 # NameRaster <- "strip_3328_cropped_rect.envi"
-NameRaster <- "strip_4159_cropped_2_rect"
+NameRaster <- "ang20190802t220708_rfl_rect"
 # Define path for image file to be processed
 Input_Image_File <- file.path(Datadir,NameRaster)
 # Define path for corresponding mask file
 # NameMask <- "strip_3328_cropped_1_merged_wa_sh_mask_rect.envi"
-NameMask <- "strip_4159_cropped_2_merged_wa_sh_mask_rect"
+NameMask <- "strip_0708_aoi_mask"
 Input_Mask_File <- file.path(Datadir, NameMask)
 # Input_Mask_File <- F
 # Define path for master output directory where files produced during the process are saved
@@ -51,8 +41,8 @@ FilterPCA <- F
 # window size for computation of spectral diversity
 window_size <- 20
 # # computational parameters
-nbCPU <- 4
-MaxRAM <- 4
+nbCPU <- 20
+MaxRAM <- 8
 # number of clusters (spectral species)
 nbclusters <- 50
 nbclusters <- 20
@@ -62,7 +52,7 @@ nbclusters <- 20
 ## https://jbferet.github.io/biodivMapR/articles/biodivMapR_4.html            ##
 ################################################################################
 print("PERFORM DIMENSIONALITY REDUCTION")
-PCA_Output <- perform_PCA(Input_Image_File = Input_Image_File,
+PCA_Output <- perform_PCA_ML(Input_Image_File = Input_Image_File,
                           Input_Mask_File = Input_Mask_File,
                           Output_Dir = Output_Dir,
                           TypePCA = TypePCA,
@@ -70,6 +60,10 @@ PCA_Output <- perform_PCA(Input_Image_File = Input_Image_File,
                           Continuum_Removal = Continuum_Removal,
                           nbCPU = nbCPU,
                           MaxRAM = MaxRAM)
+
+# save(PCA_Output, file="~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/PCA/PCA_Output.Rdata")
+get(load("~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/PCA/PCA_Output.Rdata"))
+
 
 # path for the updated mask
 Input_Mask_File <- PCA_Output$MaskPath
@@ -87,16 +81,7 @@ PCs <- rast(path)
 plot(PCs[[1:8]], col=viridis_colors, legend=F)
 # visual assesment of PCs to be done in ArcGis
 
-# Select components from the PCA/SPCA/MNF raster
-# Sel_PC = path of the file where selected components are stored
-# Sel_PC <- select_PCA_components(Input_Image_File = Input_Image_File,
-#                                 Output_Dir = Output_Dir,
-#                                 PCA_Files = PCA_Output$PCA_Files,
-#                                 TypePCA = PCA_Output$TypePCA,
-#                                 File_Open = TRUE)
-# 
-
-SelectedPCs = c(1,2,7)
+SelectedPCs = c(1,2,8,9)
 
 ################################################################################
 ##                  Perform Spectral species mapping                          ##
@@ -104,17 +89,18 @@ SelectedPCs = c(1,2,7)
 ################################################################################
 print("MAP SPECTRAL SPECIES")
 future.seed=TRUE
-Kmeans_info <- map_spectral_species(Input_Image_File = Input_Image_File,
-                                    Input_Mask_File = Input_Mask_File,
-                                    Output_Dir = Output_Dir,
-                                    SpectralSpace_Output = PCA_Output,
-                                    nbclusters = nbclusters,
-                                    nbCPU = nbCPU, MaxRAM = MaxRAM,
-                                    SelectedPCs = SelectedPCs)
+Kmeans_info <- map_spectral_species_ML(Input_Image_File = Input_Image_File,
+                                       Input_Mask_File = PCA_Output$MaskPath,
+                                       Output_Dir = Output_Dir,
+                                       SpectralSpace_Output = PCA_Output,
+                                       nbclusters = nbclusters,
+                                       nbCPU = nbCPU, MaxRAM = MaxRAM,
+                                       SelectedPCs = SelectedPCs)
+# save(Kmeans_info, file="~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/Kmeans_info_PC1289.Rdata")
+Kmeans_info <- get(load("~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/Kmeans_info_PC1289.Rdata"))
 
-
-spectral_sp_map <- rast("~/data/biodivmapR_Aviris/RESULTS/strip_3328_cropped_rect/SPCA/SpectralSpecies/SpectralSpecies")
-plot(spectral_sp_map$`Iter 2`, col=viridis_colors)
+# save(Kmeans_info, file="~/data/biodivmapR_Aviris/RESULTS_50_clusters/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/Kmeans_info_PC12.Rdata")
+# Kmeans_info <- get(load("~/data/biodivmapR_Aviris/RESULTS_50_clusters/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/Kmeans_info_PC12.Rdata"))
 
 library(RColorBrewer)
 n <- 20
@@ -130,41 +116,47 @@ plot(spectral_sp_map$`Iter 2`, col=col)
 ##                Perform alpha and beta diversity mapping                    ##
 ## https://jbferet.github.io/biodivMapR/articles/biodivMapR_6.html            ##
 ################################################################################
-
 print("MAP ALPHA DIVERSITY")
-Index_Alpha   = c('Simpson')
-Index_Alpha   = c('Shannon','Simpson')
-map_alpha_div(Input_Image_File = Input_Image_File,
-              Output_Dir = Output_Dir,
-              TypePCA = TypePCA,
-              window_size = window_size,
-              nbCPU = nbCPU,
-              MaxRAM = MaxRAM,
-              Index_Alpha = Index_Alpha,
-              nbclusters = nbclusters)
+Index_Alpha   = c('Shannon')
+# Index_Alpha   = c('Shannon','Simpson')
 
-path <- paste("~/data/biodivmapR_Aviris/RESULTS/", NameRaster, "/SPCA/ALPHA/Shannon_10", sep="")
+spectral_sp <- rast("~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/SpectralSpecies1289")
+temp_rast <- rast(ext(spectral_sp), resolution=5)
+spectral_sp_resample <- resample(spectral_sp, temp_rast)
+# writeRaster(spectral_sp_resample, filetype="ENVI", gdal="INTERLEAVE=BIL", overwrite=T, datatype="INT1U", filename="~/data/biodivmapR_Aviris/RESULTS/ang20190802t220708_rfl_rect/SPCA/SpectralSpecies/SpectralSpecies1289")
+
+map_alpha_div_ML(Input_Image_File = Input_Image_File,
+                 Output_Dir = Output_Dir,
+                 TypePCA = TypePCA,
+                 window_size = window_size,
+                 nbCPU = nbCPU,
+                 MaxRAM = MaxRAM,
+                 Index_Alpha = Index_Alpha,
+                 nbclusters = nbclusters, SelectedPCs = SelectedPCs)
+
+path <- paste(Output_Dir, "/", NameRaster, "/SPCA/ALPHA/Shannon_", window_size, "_PC", paste0(SelectedPCs, collapse = ""), sep="")
 Shannon_map <- rast(path)
-par(mfrow=c(1,2))
-plot(Shannon_map, col=viridis_colors)
-Simpson_map <- rast("~/data/biodivmapR_Aviris/RESULTS/strip_3328_cropped_rect/SPCA/ALPHA/Shannon_SD_10")
-plot(Simpson_map, col=viridis_colors)
-shannon_mean_map <- rast("~/data/biodivmapR_sent/RESULTS/sent_crop_envi/SPCA/ALPHA/Shannon_10_MeanFilter")
+plot(Shannon_map)
 
+##########
+# plotting
+##########
 
 viridis_colors <- viridis::plasma(20)
 na_col <- grey(0.8, alpha=0.5)
+plot(Shannon_map, col=viridis_colors)
+
 m <- ggplot() +
-  geom_spatraster(data = Shannon_map, na.rm = TRUE, aes(fill=Shannon_10))+
+  geom_spatraster(data = Shannon_map, na.rm = TRUE, aes(fill=Shannon_20_PC1289))+
   # theme_map()+
   theme_minimal()+
   scale_fill_gradientn(colours = viridis_colors, na.value = na_col) +
-  labs(fill = substitute(paste("Shannon index", italic("H'"))))+
-  plot_annotation(
-    title = "Map of estimates of plant Shannon diversity index",
-    theme = theme(plot.title = element_text(hjust=0.4, size = 16, face = "bold")))
-
+  labs(fill = substitute(paste("Shannon index", italic("H'"))))
+  # plot_annotation(
+    # title = "Map of estimates of plant Shannon diversity index",
+    # theme = theme(plot.title = element_text(hjust=0.4, size = 16, face = "bold")))
 m  
+
 
 map_div <- m+
   ggspatial::annotation_north_arrow(
@@ -178,22 +170,22 @@ map_div <- m+
     pad_y = unit(0.4, "in"), 
     style="ticks"
   )
+map_div
 
-path <- paste("~/data/Aviris_data/", NameRaster, sep="")
-input <- rast(path)
+path_true_col <- Input_Image_File
+input <- rast(path_true_col)
 plotRGB(input, r=names(input)[54], g=names(input)[36], b=names(input)[20], stretch="lin")
 input_scaled <-input*2500
-
-s <- ggplot() +
+t <- ggplot() +
   geom_spatraster_rgb(data = input_scaled, r=58, g=37, b=23, interpolate=T)+
   # theme_map()+
   # theme_minimal()+
   plot_annotation(
     title = "True colour map",
     theme = theme(plot.title = element_text(hjust=0.4, size = 16, face = "bold")))
-s
+t
 
-map_truecol <- s+
+map_truecol <- t+
   ggspatial::annotation_scale(location="bl", pad_x=unit(0.5, "in"),
                               pad_y = unit(0.4, "in"), style="ticks", line_col="black", text_col="black")+
   
